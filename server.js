01ebcb498db364d4343e7a3b03f574aff66dc39e
prdmnmshr <prdmnmshr@gmail.com>
2026-09-06 22:14:56 +0530

    adding updated files

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+app.use(express.json());
 const http = require('http');
 const PORT = 8080
+
+const webhookRoutes = require('./src/routes/webhookRoute');
 
 const server = http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type':'text/plain' });
     res.end('Hello World\n')
 })
+
+app.use('api/webhook',webhookRoutes)
+
 server.listen(PORT,() => {
+    console.log(process.env.GITHUB_TOKEN)
     console.log(`Server running at http://localhost:${PORT}`);
-})
+});

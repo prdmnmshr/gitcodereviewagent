53b7b80a888de889ddc3071467c8b299738a26fb
prdmnmshr <prdmnmshr@gmail.com>
2026-09-06 17:50:53 +0530

    checking

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.get('/',(req,res)=>{
 
 });
 app.post('/webhook',(req,res)=>{
-    console.log('Webhook received', req.body)
+    console.log('Webhook received')
+    console.log(req.body)
     res.status(200).json({
         message: " webhook received successfully"
     })

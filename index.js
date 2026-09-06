29392ebf8b4593762e6a33022979d417531bf19a
prdmnmshr <prdmnmshr@gmail.com>
2026-09-06 17:57:28 +0530

    added

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ app.get('/',(req,res)=>{
     res.send('hello world is working fine')
 
 });
-app.post('/webhook',(req,res)=>{
-    console.log('Webhook received')
-    console.log(req.body)
-    res.status(200).json({
-        message: " webhook received successfully"
-    })
+app.post('/webhook', (req, res) => {
+    console.log('Webhook received', req.body);
 
-})
+    res.status(200).json({
+        message: "webhook received successfully"
+    });
+
+});
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
